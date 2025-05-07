6d7bf443b3c21bd7b8c83c46c7639474c543f190
Max Starr <max@maxstarr.xyz>
2025-05-06 22:39:55 -0500

    better colors

--- src/lib/color.ts
+++ src/lib/color.ts
@@ -1,5 +1,5 @@
 export function getColor(index: number, max: number): string {
   const ratio = index / max;
-  const hue = Math.floor(ratio * 120);
-  return `hsl(${hue}, 100%, 50%)`;
+  const hue = Math.floor(ratio * 360);
+  return `hsl(${hue}, 80%, 80%)`;
 }
